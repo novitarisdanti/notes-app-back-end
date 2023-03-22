7654909c68ae36185d82eec6deacc4d06518a98e
novitarise@gmail.com <you@example.com>
2023-03-22 18:57:01 +0700

    first commit

---
+++ notes.js
@@ -0,0 +1,15 @@
+//array untuk menampung objek catatan
+
+const notes = [
+    {
+        id: string,
+        title: string,
+        createdAt: string,
+        updatedAt: string,
+        tags: "array of string",
+        body: string,
+       },
+   
+];
+ 
+module.exports = notes;
